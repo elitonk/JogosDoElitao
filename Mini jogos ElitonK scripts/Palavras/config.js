0091c0f34152607de5d31841c46e6733a86c1182
Eliton da Silva <73960969+elitonk@users.noreply.github.com>
2022-07-09 01:36:29 -0300

    Update config.js

--- Mini jogos ElitonK scripts/Palavras/config.js
+++ Mini jogos ElitonK scripts/Palavras/config.js
@@ -1,12 +1,12 @@
 ﻿var settings = {
-  "liveOnly": false,
+  "liveOnly": true,
   "permission": "Everyone",
   "ignoreCaseSensitivity": true,
   "newWordOnAnswer": false,
   "minWordInterval": 9,
   "maxWordInterval": 19,
   "minReward": 4,
-  "maxReward": 17,
+  "maxReward": 20,
   "responseAnnouncement": "Quem escrever $palavra primeiro ganha $recompensa $moeda!",
   "responseWon": "$user escreveu primeiro $palavra e ganhou $recompensa $moeda!"
 };
